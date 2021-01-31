9ed0c0fb811bd575f24bff8fca3d8c50acb49aa5
unknown <964746175@qq.com>
2021-02-01 02:11:33 +0800

    改为分布式爬虫

---
+++ jd_book/pipelines.py
@@ -0,0 +1,17 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
+from datetime import datetime
+
+
+class JdBookPipeline:
+    def process_item(self, item, spider):
+        item["crawled"] = datetime.utcnow()
+        item["spider"] = spider.name
+        return item
